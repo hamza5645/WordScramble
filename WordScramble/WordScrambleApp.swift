be56d97920ec19bf3bdf8c17743f91f0e8d7e669
hamza5645 <hamzaosama5645@outlook.com>
2022-08-28 11:24:30 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Hamza Osama on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
